91b5704098b0152daa397d238b90238e2b6afe69
devclass1 <72622124+devclass1@users.noreply.github.com>
2022-12-12 12:19:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,3 +9,4 @@ printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-
 printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
 
 printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+?>
